570935b5ecb7dcd6491ad739fd4ef38e0be9b000
baladitya <baladityasanaboina12@gmail.com>
2025-01-03 13:28:30 +0530

    updated with os port

--- app.py
+++ app.py
@@ -146,4 +146,5 @@ def send_static(path):
     return send_from_directory('static', path)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
